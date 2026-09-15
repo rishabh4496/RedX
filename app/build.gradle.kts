869409c99ed4ee4095ad1edd35ff4ae878a3d46d
rishabh4496 <103663851+rishabh4496@users.noreply.github.com>
2026-09-15 23:18:37 +0530

    build: publish full 16.8MB unminified v2.2.0 release binaries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             signingConfig = signingConfigs.getByName("debug")
         }
